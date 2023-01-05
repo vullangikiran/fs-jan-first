fd1bb4c7fb1c5f32f07ee60e376662294d87918c
vullangikiran <vullangikiran123@gmail.com>
2023-01-05 16:55:50 +0530

    text

---
+++ four.js
@@ -0,0 +1,12 @@
+var a=10
+var b=20
+var c=30.5
+var d=40
+var e='Reddy'
+var f="gang"
+var g=true
+var h=false
+var i
+
+console.log(a+b)
+console.log(a+c)
---
+++ two.js
@@ -0,0 +1,20 @@
+var a=10
+var b=20
+var c=30.5
+var d=40
+var e='Reddy'
+var f="gang"
+var g=true
+var h=false
+var i
+
+console.log(a+b)
+console.log(a+c)
+console.log(a+e)
+console.log(a+f)
+console.log(a+g)
+console.log(a+h)
+console.log(a+b)
+console.log(e+f)
+console.log(a+i)
+console.log(g+h)
