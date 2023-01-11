ee662289962c124d5c7fb09f4dc62354b5aea982
vullangikiran <vullangikiran123@gmail.com>
2023-01-11 22:08:34 +0530

    terxt

---
+++ Arrays/nine.js
@@ -0,0 +1,19 @@
+let a=[10,20,30,40];
+
+a.push(50);
+console.log(a)
+
+a.pop()
+console.log(a)
+
+a.unshift(5)
+console.log(a)
+
+a.shift()
+console.log(a)
+
+a.slice()
+console.log(a)
+
+a.reverse()
+console.log(a)
