138aede60374e7dd8c6337b4e5cf9ac7c723607a
vullangikiran <vullangikiran123@gmail.com>
2023-01-11 21:43:23 +0530

    text

---
+++ objects/six.js
@@ -0,0 +1,16 @@
+let product={
+    id:'p101',
+    name:'iphone14pro',
+    price:135000,
+    color:['grey','black','Gold']
+}
+console.log(product)
+
+for(pro in product){
+    console.log(product[pro])
+}
+
+
+
+
+    console.log(product.color[0])
