d49c338ec10d919f37cd5fa004a5277585740aa9
vullangikiran <vullangikiran123@gmail.com>
2023-01-27 10:37:02 +0530

    text

---
+++ WEB/js-1/Arrays/onn.js
@@ -0,0 +1,5 @@
+let arr=[10,20,30,40]
+for(  a of arr)
+{
+    console.log(arr[a])
+}
---
+++ WEB/js-2/one.js
@@ -0,0 +1,8 @@
+function display(){
+    console.log("good morning")
+}
+
+display()
+display()
+display()
+display()
